f512de7be1f42cb409560942f3dc651b4f600f5d
murad564 <79305698+murad564@users.noreply.github.com>
2022-09-29 16:20:19 +0400

    Create JsonFormat.cs

---
+++ Exam_Boss.az/JsonFormat.cs
@@ -0,0 +1,20 @@
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+
+// namespace SearchingSYS
+//{
+//    class JsonFormat
+//    {
+//        public static void WriteToJsonFile<T>(T obj,string path)
+//        {
+//            var options = new JsonSerializerOptions(); options.WriteIndented = true;
+//            var textJson = JsonSerializer.Serialize(obj, options);
+//            File.WriteAllText(path, textJson);
+//        }
+//    }
+//}
